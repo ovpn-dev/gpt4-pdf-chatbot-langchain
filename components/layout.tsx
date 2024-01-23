cd9c2d7ce30bd4e5829917994aa4a988487e69f3
ovpn-dev <ovpn05@gmail.com>
2024-01-23 15:12:44 +0100

    to test vercel

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,15 +1,25 @@
+import Image from 'next/image';
+
 interface LayoutProps {
   children?: React.ReactNode;
 }
 
 export default function Layout({ children }: LayoutProps) {
   return (
-    <div className="mx-auto flex flex-col space-y-4">
-      <header className="container sticky top-0 z-40 bg-white">
-        <div className="h-16 border-b border-b-slate-200 py-4">
+    <div className="mx-auto flex flex-col space-y-4 bg-slate-300">
+      <header className="container sticky top-0 z-40 bg-slate-300">
+        <div className="h-16 border-b border-b-slate-300 py-4">
           <nav className="ml-4 pl-6">
-            <a href="#" className="hover:text-slate-600 cursor-pointer">
-              Home
+            <a
+              href="https://latinbrains.com/"
+              className="hover:text-slate-600 cursor-pointer"
+            >
+              <Image
+                src={'/latin-logo-22.png'}
+                width={180}
+                height={180}
+                alt="Latin Brains Logo"
+              />
             </a>
           </nav>
         </div>
